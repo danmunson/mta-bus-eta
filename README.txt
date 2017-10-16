bf436263764bab9a2e15723d5634434e593caba3
Dan Munson <dansmunson@gmail.com>
2017-10-15 23:11:54 -0400

    run.py

--- README.txt
+++ README.txt
@@ -1,3 +1,13 @@
+MTA Bus ETA project
+The function of this project is to develop models that can accurately estimate the time until 
+the next bus arrives, for each stop along a given route. Data for each stop is gathered by 
+periodically scraping a webpage (see: Part I sample URLs) that gives the current position 
+of all buses along the route. These models can and will be benchmarked against the MTA's 
+own predictive models, which are also public. It remains to be seen who will emerge victorious...
+
+
+PART I: Collecting raw data, transforming it for use in constructing models
+
 Steps for Gathering data from MTA bus sites:
 1)  Setup directory system for a route by running [config_new_route.py], and following prompts
 2)  Schedule & execute a collection process by running [schedule_collection.py], and following the prompts
@@ -8,9 +18,6 @@ NOTE--> Important assumptions in data collection process:
     a. No two buses will ever occupy the same stop position
     b. 3 buses will never be at 3 adjacent stops simultaneously
     c. In each successive "snapshot" the bus must be in the same position as the previous, or the next position
-
-NOTE--> After building a model (module coming soon), predicted ETAs can be compared against MTA's provided ETAs (found by clicking link on bus stop name)
-
 
 SAMPLE URLS:
 M60SBS route: https://bustime.mta.info/m/?q=M60-SBS
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#run
 import requests as req
 import io
 import pandas as pd
@@ -7,9 +8,58 @@ import unicodedata
 import os, shutil
 from BusETA import DataTransforms
 from BusETA import DataCollection
+import sys
 
-
+cursor = DataTransforms.Transforms.cursor
+prepper = DataTransforms.Transforms.prepare_dataframe
+dirsplitter = DataTransforms.Transforms.split_directions
+normalize = DataCollection.GetBusData.normalize
 capture = DataCollection.GetBusData.capture_bus_page
+
+
+def process_data():
+    #fixes an encoding problem
+    reload(sys)  
+    sys.setdefaultencoding('utf8')
+
+    route_df = pd.read_csv('./route_dict.csv', header=None)
+    routes = list(route_df.ix[:,0])
+
+    print '** Commence processing **'
+
+    for route in routes:
+
+        raw_data_path = 'Routes/'+route+'/stopdata.csv'
+
+        posdf = pd.read_csv('Routes/'+route+'/positioning.csv', header=None)
+        rawdf = prepper(raw_data_path, posdf)
+        dir_wise, stop_names = dirsplitter(rawdf, 'approaching')
+
+        for i in range(len(dir_wise)):
+            dir_df = dir_wise[i]
+            dir = dir_df.ix[0,'Direction']
+
+            stop_names = list(set(list(dir_df.ix[:,'Stop'])))
+            for stop in stop_names:
+                stopnm = stop
+                accum = [] #columns=['Position', 'Status', 'Timestamp', 'TimeDelta'])
+
+                #process data for a given stop
+                cursor(dir_df, accum, stopnm, 'approaching', 0)
+
+                final_df = pd.DataFrame(accum)
+                if final_df.shape[0] != 0:
+                    final_df = final_df.sort_values(by='TimeDelta')
+                    savefile = 'Routes/'+route+'/'+normalize(dir)+'/'+normalize(stopnm)+'/'+unicode(datetime.datetime.now())+'.csv'
+                    final_df.to_csv(savefile, index=False)
+
+        #clear data so it can't accidentally be processed twice
+        rawfile = io.open(raw_data_path,'w')
+        rawfile.close()
+
+    print '** Processing complete **'
+    return
+
 
 #batch interval represents the duration for one batch of routes to be scraped continuously
 #suggested batch interval = 25 min
@@ -31,18 +81,20 @@ MAX_BATCH_SIZE = int(SI_sec/0.3) #determined by assuming that each scraping of a
 route_dict = pd.read_csv('./route_dict.csv', header=None)
 num_routes = route_dict.shape[0]
 counter = num_routes # this counter is used to generate a new batch at each cycle
-#setup stopdata csv
-route_names = list(route_dict.ix[:,0])
-for name in route_names:
-    csv = io.open('Routes/' + name + '/stopdata.csv', 'w')
-    csv.write(unicode('Direction,Stop,Status,Time\n'))
-
 
 ## Step 2: Loop through batches, passing each batch to the scrape cycler
+for i in range(total_iterations):
+    print i
 
 for num in range(total_iterations):
     #generate batch by cycling thru the route_dict in intervals of BATCH_SIZE
     
+    route_names = list(route_dict.ix[:,0])
+    for name in route_names:
+        csv = io.open('Routes/' + name + '/stopdata.csv', 'w')
+        csv.write(unicode('Direction,Stop,Status,Time\n'))
+        csv.close()
+
     batch_routes = []
     batch_size = min(MAX_BATCH_SIZE, route_dict.shape[0])
     for i in range(batch_size):
@@ -70,5 +122,6 @@ for num in range(total_iterations):
         batch_duration = datetime.datetime.now() - batch_start_time
     
     print '* Batch ' + str(num) + ' complete *'
+    process_data()
 
 print '** Collection process complete **'
