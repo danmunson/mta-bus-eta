5565a56fb9986187473ce4599c03fdf6f34f803b
Dan Munson <dansmunson@gmail.com>
2017-11-16 22:32:00 -0500

    misc

--- Server.py
+++ Server.py
@@ -13,45 +13,45 @@ get_lm_pred_input = Modeling.Persistence.get_lm_prediction_input
 get_categ_pred_input = Modeling.Persistence.get_categ_prediction_input
 
 def return_prediction(routename, direction, stop, current_model_type):
-    #try:
-    stop_path = os.path.join('Routes', os.path.join(routename, os.path.join(direction, stop)))
-    model = get_model(stop_path)
+    try:
+        stop_path = os.path.join('Routes', os.path.join(routename, os.path.join(direction, stop)))
+        model = get_model(stop_path)
 
-    route_dict = pd.read_csv('route_dict.csv', header=None)
-    source_url = None
-    for i in range(route_dict.shape[0]):
-        if route_dict.ix[i,0] == routename:
-            source_url = route_dict.ix[i,1]
+        route_dict = pd.read_csv('route_dict.csv', header=None)
+        source_url = None
+        for i in range(route_dict.shape[0]):
+            if route_dict.ix[i,0] == routename:
+                source_url = route_dict.ix[i,1]
 
-    position_df = pd.read_csv('Routes/'+routename+'/positioning.csv', header=None)
-    live_data = get_live_data(source_url, position_df, direction, stop)
+        position_df = pd.read_csv('Routes/'+routename+'/positioning.csv', header=None)
+        live_data = get_live_data(source_url, position_df, direction, stop)
 
-    if live_data['postat'] == 'XXXX':   #indicates there are not enough earlier stops to make a prediction
-        return 'No nearby buses. Try again soon.'
-    
-    pred_vec = None
-    pred_dict = None
-    prediction = None
+        if live_data['postat'] == 'XXXX':   #indicates there are not enough earlier stops to make a prediction
+            return 'No nearby buses. Try again soon.'
+        
+        pred_vec = None
+        pred_dict = None
+        prediction = None
 
-    if current_model_type == 'lm':
-        metafile = io.open(stop_path+'/model_columns.txt', 'r')
-        pred_vec, pred_dict, postat_bool = get_lm_pred_input(live_data, metafile)
-        metafile.close()
-        if not postat_bool:
-            return 'Bus out of range. Try again soon.'
-        else:
-            prediction = model.preddict(pred_vec)
-    elif current_model_type == 'categ':
-        try:
-            pred_vec = get_categ_pred_input(live_data)
-            prediction = model.predict(pred_vec)
-        except Exception as e:
-            return 'Bus out of range. Try again soon.'
-    
-    mins = round(prediction[0]/60, 1)
-    return str(mins) + 'min : ' + str(pred_dict)
-    #except Exception as e:
-    #    return 'Exception: ' + str(e)
+        if current_model_type == 'lm':
+            metafile = io.open(stop_path+'/model_columns.txt', 'r')
+            pred_vec, pred_dict, postat_bool = get_lm_pred_input(live_data, metafile)
+            metafile.close()
+            if not postat_bool:
+                return 'Bus out of range. Try again soon.'
+            else:
+                prediction = model.preddict(pred_vec)
+        elif current_model_type == 'categ':
+            try:
+                pred_vec = get_categ_pred_input(live_data)
+                prediction = model.predict(pred_vec)
+            except Exception as e:
+                return 'Bus out of range. Try again soon.'
+        
+        est_mins = round(prediction[0]/60, 1)
+        return str(mins) + ' minutes.'
+    except Exception as e:
+        return 'Sorry, something went wrong.'
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
