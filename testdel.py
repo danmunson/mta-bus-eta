a6d69c9c783477f9d986f75a6badd2ee9fad9aff
Dan Munson <dansmunson@gmail.com>
2017-12-02 21:25:21 -0500

    test_del

--- testdel.py
+++ testdel.py
@@ -9,6 +9,8 @@ for r, ds, fs in os.walk('Routes'):
     for f in fs:
         name, ext = os.path.splitext(f)
         if ext == '.csv':
+            if name == 'stopdata' or name == 'positioning':
+                continue
             timestamp = dt.datetime.strptime(name, '%Y-%m-%d %H:%M:%S.%f')
             delta = dt.datetime.now() - timestamp
             if delta.total_seconds() > (24 * 60 * 60 * MAX_DAYS):
