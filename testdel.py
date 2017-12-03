0706f725ae089b4abe9e8fcfae432d6db349eb0a
Dan Munson <dansmunson@gmail.com>
2017-12-02 21:23:13 -0500

    test_del

--- testdel.py
+++ testdel.py
@@ -5,11 +5,11 @@ import datetime as dt
 
 MAX_DAYS = 21
 
-for r, ds, fs in os.walk('../'):
+for r, ds, fs in os.walk('Routes'):
     for f in fs:
         name, ext = os.path.splitext(f)
         if ext == '.csv':
-            timestamp = dt.datetime.strptime(name, format='%Y-%m-%d %H:%M:%S.%f')
+            timestamp = dt.datetime.strptime(name, '%Y-%m-%d %H:%M:%S.%f')
             delta = dt.datetime.now() - timestamp
             if delta.total_seconds() > (24 * 60 * 60 * MAX_DAYS):
                 print 'OLD'
