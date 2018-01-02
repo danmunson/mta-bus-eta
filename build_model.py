da533882ee6c12ef14de3d9f199b0c91bbf7f62f
Dan Munson <dansmunson@gmail.com>
2018-01-02 17:33:07 -0500

    modding main cycle

--- build_model.py
+++ build_model.py
@@ -2,7 +2,7 @@ import io, os, shutil, unicodedata, datetime
 from BusETA import Modeling as Md
 from BusETA import Algorithms
 
-MODEL_OBJ = Algorithms.MedianLookup
+MODEL = Algorithms.MedianLookup
 def train_new_models():
     route_names = []
     for root, ds, fs in os.walk('Routes'):
@@ -11,7 +11,8 @@ def train_new_models():
     for route in route_names:
         stopdfs = Md.Read.read_all_stops(route)
         finished_dfs = Md.FeatureEng.apply_SDT(stopdfs)
-        Md.Persistence.train_save_route(finished_dfs, MODEL_OBJ)
+        model = MODEL()
+        Md.Persistence.train_save_route(finished_dfs, model)
     return
 
 train_new_models()
