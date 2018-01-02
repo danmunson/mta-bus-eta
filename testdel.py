fb1dfbe328e2427b0ba3f0b9434edb3ffba09c59
Dan Munson <dansmunson@gmail.com>
2018-01-02 13:08:43 -0500

    modifying main cycle

--- testdel.py
+++ testdel.py
@@ -14,5 +14,6 @@ for r, ds, fs in os.walk('Routes'):
             timestamp = dt.datetime.strptime(name, '%Y-%m-%d %H:%M:%S.%f')
             delta = dt.datetime.now() - timestamp
             if delta.total_seconds() > (24 * 60 * 60 * MAX_DAYS):
-                pathname = name + ext
+                pathname = os.path.join(r, f)
                 os.remove(pathname)
+                print '-del'
